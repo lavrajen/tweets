4630dd9c582d97f6f1ccdd63976f705b675d66d7
Lavanya Rajendran <lavraje@amazon.co.uk>
2024-09-17 09:41:49 +0100

    Created a timeline

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,10 @@ class MainController < ApplicationController
   def index
     # flash.now[:notice] = "Logged in successfully"
     # flash.now[:alert] = "Invalid email or password"
+    def index
+      # where(user: Current.user.following) -- add this when we have a follow feature
+      @alltweets = Tweet.order(created_at: :desc)
+    end
     if session[:user_id]
       # using find_by instead of find means that the rails app will not crash when the user can't be found
       @user = User.find_by(id: session[:user_id])
